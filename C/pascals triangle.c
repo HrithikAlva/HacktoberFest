aab1b693af9d3bf9eac28d3ac1f7f2e61176a052
HrithikAlva <73232234+HrithikAlva@users.noreply.github.com>
2020-10-30 21:58:54 +0530

    Update pascals triangle.c

--- C/pascals triangle.c
+++ C/pascals triangle.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
-
-int factorial(int n) {
-   int f;
-   
-   for(f = 1; n > 1; n--)
-      f *= n;
-      
-   return f;
-}
-
-int ncr(int n,int r) {
-   return factorial(n) / ( factorial(n-r) * factorial(r) );
-}
-
 int main() {
-   int n, i, j;
-
-   n = 5;
-
-   for(i = 0; i <= n; i++) {
-      for(j = 0; j <= n-i; j++)
+   int rows=5, coef = 1, space, i, j;
+   for (i = 0; i < rows; i++) {
+      for (space = 1; space <= rows - i; space++)
          printf("  ");
-         
-      for(j = 0; j <= i; j++)
-         printf(" %3d", ncr(i, j));
-
+      for (j = 0; j <= i; j++) {
+         if (j == 0 || i == 0)
+            coef = 1;
+         else
+            coef = coef * (i - j + 1) / j;
+         printf("%4d", coef);
+      }
       printf("\n");
    }
    return 0;
