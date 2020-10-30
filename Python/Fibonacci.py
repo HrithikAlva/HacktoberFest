de2de282edc161fdb7759ef46afac3461da12b2a
HrithikAlva <73232234+HrithikAlva@users.noreply.github.com>
2020-10-30 21:05:16 +0530

    Update Fibonacci.py

--- Python/Fibonacci.py
+++ Python/Fibonacci.py
@@ -1,29 +1,20 @@
-def fibUsingLoop(n):
-
-    first = 0
-    second = 1
-    third = first + second
-
-    for i in range(n):
-        print(first)
-
-        first = second
-        second = third
-        third = first + second
-
-def fibUsingRecursion(n):
-
-   if n<0: 
-        print("Incorrect input") 
-   elif n==0: 
-        return 0
-   elif n==1: 
-        return 1
-   else: 
-        return fibUsingRecursion(n-1)+fibUsingRecursion(n-2)
-n = int(input("How many numbers from fib series? : "))
-
-fibUsingLoop(n)
-print(fibUsingRecursion(n)) 
-
-
+# Program to display the Fibonacci sequence up to n-th term
+nterms = int(input("How many terms? "))
+# first two terms
+n1, n2 = 0, 1
+count = 0
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
